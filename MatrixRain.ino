b7cff3b3e057d8df71311c47a8b0a212b4c050f7
Muten-Roshi-Sama <131618669+Muten-Roshi-Sama@users.noreply.github.com>
2025-02-08 01:54:01 +0100

    Update MatrixRain.ino

--- MatrixRain.ino
+++ MatrixRain.ino
@@ -1,21 +1,40 @@
+#include <Arduino.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
 #include "DigitalRainAnim.h"
 
+
+// TFT object initialization
 TFT_eSPI tft = TFT_eSPI();
+
 DigitalRainAnim digitalRainAnim = DigitalRainAnim(); 
- 
+
+// **User-defined animation speed variables**
+int new_line_len_min = 5;
+int new_line_len_max = 18;
+int min_line_speed = 3;  // Minimum speed of falling characters (higher = slower)
+int max_line_speed = 5;  // Maximum speed of falling characters (higher = slower)
+int frame_delay = 20;    // Time delay per frame (lower = faster animation)
+
+
+
 void setup() {
   Serial.begin(115200);
-//  SPIFFS.begin();
-//  listFiles(); // Lists the files so you can see what is in the SPIFFS
-
   tft.begin();
-  tft.setRotation(2);
+  tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
-  digitalRainAnim.init(&tft);
+  digitalRainAnim.init(&tft, new_line_len_min, new_line_len_max, min_line_speed, max_line_speed, frame_delay);
 }
 
 void loop() {
+  
+  if(millis()>5000) digitalRainAnim.stop();
+
+  if(millis()>7000) digitalRainAnim.resume();
+
+
   digitalRainAnim.loop();
 }
+
+
+
